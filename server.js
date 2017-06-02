f95aea3cc4aca592979be1c94e4f6ff58fe04981
nemosaurus <nemosaurus@gmail.com>
2017-06-02 01:29:40 -0700

    films created

--- server.js
+++ server.js
@@ -26,22 +26,56 @@ router.get('/', function(req, res) {
   res.json({ message: 'Please see API documentation' });
 });
 
+router.route('/films')
+// get all the people (accessed at GET http://localhost:8080/api/people)
+.get(function(req, res) {
+  Films.find(function(err, films) {
+    if (err)
+      res.send(err);
+
+    res.json(films);
+  });
+})
+// create a film (accessed at POST http://localhost:8080/api/film)
+.post(function(req, res) {
+  var film = new Films();
+  film.title         = req.body.title;
+  film.episode_id    = req.body.episode_id;
+  film.opening_crawl = req.body.opening_crawl;
+  film.director      = req.body.director;
+  film.producer      = req.body.producer;
+  film.release_date  = req.body.release_date;
+  film.characters    = req.body.characters;
+  film.planets       = req.body.planets;
+  film.species       = req.body.species;
+  film.vehicles      = req.body.vehicles;
+  film.starships     = req.body.starships;
+  film.url           = req.body.url;
+  film.created       = req.body.created;
+  film.edited        = req.body.edited;
+  // save the film and check for errors
+  film.save(function(err) {
+    if (err)
+    res.send(err);
+
+    res.json({ message: 'film created!' });
+  });
+});
+
 
 router.route('/people')
 // get all the people (accessed at GET http://localhost:8080/api/people)
 .get(function(req, res) {
   People.find(function(err, people) {
     if (err)
-    res.send(err);
+      res.send(err);
 
     res.json(people);
-
   });
 })
 // create a person (accessed at POST http://localhost:8080/api/people)
 .post(function(req, res) {
   var people = new People();
-  console.log(req);
   people.name       = req.body.name;
   people.height     = req.body.height;
   people.mass       = req.body.mass;
